e8c3190b630e43ee17269bb74694c3a4ecc7dafc
Scott Brown <brownsf@gmail.com>
2021-06-28 17:12:39 -0500

    fix spacing

--- pages/admin.js
+++ pages/admin.js
@@ -13,7 +13,8 @@ const useStyles = makeStyles((theme) => ({
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    paddingBottom: theme.spacing(1)
+    paddingBottom: theme.spacing(1),
+    width: "100%"
   }
 }));
 
