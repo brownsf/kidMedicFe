7dd4cd0bcaa1f2cd685301fd7f8846d2e9ed3110
brownsf <brownsf@gmail.com>
2021-08-21 12:28:30 -0500

    admin

---
+++ pages/admin/index.js
@@ -0,0 +1,11 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+export default function Profile({ user }) {
+    console.log({user})
+    console.log(user.user_id)
+  return <div>Hello {user.name}</div>;
+}
+
+// You can optionally pass your own `getServerSideProps` function into
+// `withPageAuthRequired` and the props will be merged with the `user` prop
+export const getServerSideProps = withPageAuthRequired();
